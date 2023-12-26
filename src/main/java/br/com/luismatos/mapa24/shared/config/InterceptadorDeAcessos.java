523bddebf5a97ee1af9eb5839acd91505249df06
LouisMatos <luismatos000@gmail.com>
2023-12-26 07:17:16 -0300

    melhorias

---
+++ src/main/java/br/com/luismatos/mapa24/shared/config/InterceptadorDeAcessos.java
@@ -0,0 +1,82 @@
+package br.com.luismatos.mapa24.shared.config;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import br.com.luismatos.mapa24.shared.logger.log.Logger;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class InterceptadorDeAcessos implements HandlerInterceptor {
+	
+	
+	public static List<Acesso> acessos = new ArrayList<Acesso>();
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		Acesso acesso = new Acesso();
+
+		acesso.path = request.getRequestURI();
+		acesso.data = LocalDateTime.now();
+
+		request.setAttribute("acesso", acesso);
+
+		return true;
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		Acesso acesso = (Acesso) request.getAttribute("acesso");
+		acesso.duracao = Duration.between(acesso.data, LocalDateTime.now());
+		acessos.add(acesso);
+		log.info(acesso.toString());
+	}
+
+	public static class Acesso {
+		
+		private String path;
+		private LocalDateTime data;
+		private Duration duracao;
+
+		public String getPath() {
+			return path;
+		}
+
+		public void setPath(String path) {
+			this.path = path;
+		}
+
+		public LocalDateTime getData() {
+			return data;
+		}
+
+		public void setData(LocalDateTime data) {
+			this.data = data;
+		}
+
+		public Duration getDuracao() {
+			return duracao;
+		}
+
+		public void setDuracao(Duration duracao) {
+			this.duracao = duracao;
+		}
+
+		@Override
+		public String toString() {
+			
+			return "Acesso [path=" + path + ", data=" + data + ", duracao=" + duracao + "]";
+		}
+
+	}
+
+}
